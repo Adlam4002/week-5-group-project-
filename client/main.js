06875012312151687ba106afeb67c5bb161d6b99
Daniel Szabo <d.g.szabo-2017@hull.ac.uk>
2024-06-17 15:59:40 +0200

    Add form submission functionality to client/main.js

--- client/main.js
+++ client/main.js
@@ -1,1 +1,29 @@
-console.log("Hello world!");
+// Function to add a task and fetch the data to the server
+async function addnewTask(event) {
+  event.preventDefault();
+  console.log("Task submitted!");
+  // Get the form data and convert it to an object to be sent to the server
+  const formData = Object.fromEntries(new FormData(formTask));
+  console.log(formData);
+  // Send the data to the server
+  try {
+    // Fetch the data from the server
+    await fetch(`${LINK}/submit`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    });
+  } catch (error) {
+    // Error handling
+    console.error("Error submitting task!", error);
+  }
+}
+
+// Get the form from the page
+const formTask = document.getElementById("form-task");
+// Link for the API database
+const LINK = "http://localhost:8080"; // To be changed with the Render URL
+// Add an event listener to the form
+formTask.addEventListener("submit", addnewTask);
